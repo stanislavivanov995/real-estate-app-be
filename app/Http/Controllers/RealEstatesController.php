226a4e292d4115383a95df4c32b3e7d5377c328b
Kosta Kazakov <kostakazakoff@gmail.com>
2023-10-26 23:04:49 +0300

    working on controller - image upload

--- app/Http/Controllers/RealEstatesController.php
+++ app/Http/Controllers/RealEstatesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use App\Models\RealEstate;
 use Illuminate\Http\JsonResponse;
 use App\Http\Requests\StoreEstateRequest;
@@ -24,11 +25,22 @@ class RealEstatesController extends Controller
         return response()->json(['Valid' => $realEstate]);
     }
 
-    public function store(StoreEstateRequest $request): JsonResponse
+    public function store(StoreEstateRequest $request, Image $image): JsonResponse
     {
         $data = $request->json()->all();
 
         $realEstate = RealEstate::create($data);
+
+        if (count($request->files) > 0) {
+            dd($request->files);
+
+            foreach ($request->files as $file) {
+                $file->store('images', 'public');
+                $image = Image::create($file);
+                $id = $image['id'];
+                $realEstate['to_images'] = $id;
+            }
+        }
 
         return response()->json(['Valid' => $realEstate]);
     }
