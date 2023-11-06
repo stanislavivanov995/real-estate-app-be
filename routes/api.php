809138d0e54d335dc8b6748efeb137ea2bd1a4c9
Kosta Kazakov <kostakazakoff@gmail.com>
2023-11-06 18:30:19 +0200

    Category/categories model/table created

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/categories', [CategoriesController::class, 'getCategories']);
+Route::get('/list-categories', [CategoriesController::class, 'getCategories']);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
