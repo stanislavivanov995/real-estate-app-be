ba57946efdc10637d40635cfaa0ebc51efda4e98
Kosta Kazakov <kostakazakoff@gmail.com>
2023-11-06 19:40:28 +0200

    Category seedre created

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    public function run(): void
+    {
+        Category::create([
+            'name' => 'Hotel',
+        ]);
+        Category::create([
+            'name' => 'Apartament',
+        ]);
+        Category::create([
+            'name' => 'House',
+        ]);
+        Category::create([
+            'name' => 'Hostel / room',
+        ]);
+        Category::create([
+            'name' => 'Camping',
+        ]);
+    }
+}
