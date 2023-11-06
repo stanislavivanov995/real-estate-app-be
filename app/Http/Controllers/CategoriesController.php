a196c5121c2823fdfb23557664602be6555b27de
Kosta Kazakov <kostakazakoff@gmail.com>
2023-11-06 21:38:06 +0200

    refactoring

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use Illuminate\Http\JsonResponse;
+
 // use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function getCategories()
+    public function getCategories(): JsonResponse
     {
         $list = Category::all();
 
