e89129d8c936e6b757f332900c2029737c641357
Kosta Kazakov <kostakazakoff@gmail.com>
2023-11-06 16:49:17 +0200

    route to Categories checked

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/categories/check', [CategoriesController::class, 'check']);
+Route::get('/categories', [CategoriesController::class, 'check']);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
