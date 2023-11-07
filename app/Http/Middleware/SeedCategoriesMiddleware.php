9ba7a93b1a7ad7cdae9994d6fcde14d9b830f860
Kosta Kazakov <kostakazakoff@gmail.com>
2023-11-07 12:42:07 +0200

    refactoring

--- app/Http/Middleware/SeedCategoriesMiddleware.php
+++ app/Http/Middleware/SeedCategoriesMiddleware.php
@@ -10,7 +10,6 @@ class SeedCategoriesMiddleware
 {
     public function handle($request, Closure $next)
     {
-        // Check if the categories table is empty
         if (DB::table('categories')->count() === 0) {
             Artisan::call('db:seed', [
                 '--class' => 'CategorySeeder',
