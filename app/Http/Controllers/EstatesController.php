1ec1145c5555f30f8966ef5a3cdefcfdd58fc288
Kosta Kazakov <kostakazakoff@gmail.com>
2023-11-11 01:18:50 +0200

    Estates roots and controllers created

---
+++ app/Http/Controllers/EstatesController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Estate;
+use Illuminate\Http\JsonResponse;
+use App\Http\Requests\StoreEstateRequest;
+
+class EstatesController extends Controller
+{
+    public function list(): ?JsonResponse
+    {
+        $list = Estate::all()->orderBy('created_at', 'updated_at', 'desc');
+        $softDeleted = Estate::onlyTrashed()->get();
+
+        return response()->json(['estates' => $list, 'deleted' => $softDeleted]);
+    }
+
+
+    public function show(string $id): ?JsonResponse
+    {
+        $estate = Estate::findOrFail($id);
+
+        return response()->json(['estate' => $estate]);
+    }
+
+
+    public function store(StoreEstateRequest $request): ?JsonResponse
+    {
+        $estate = Estate::create([
+            'user_id' => $request->user_id,
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'currency' => $request->currency,
+            'latitude' => $request->latitude,
+            'longtitude' => $request->longtitude,
+            'category' => $request->category_id,
+            'rooms' => $request->rooms,
+            'arrive_hour' => $request->arrive_hour,
+            'leave_hour' => $request->leave_hour
+        ]);
+
+        return response()->json(["success" => true, 'estate' => $estate]);
+    }
+
+
+    public function update(StoreEstateRequest $request, string $id): ?JsonResponse
+    {
+        $estate = Estate::findOrFail($id);
+
+        $estate->update([
+            'user_id' => $request->user_id,
+            'name' => $request->name,
+            'description' => $request->description,
+            'rooms' => $request->rooms,
+            'price' => $request->price,
+            'currency' => $request->currency,
+            'latitude' => $request->latitude,
+            'longtitude' => $request->longtitude,
+            'category_id' => $request->category_id,
+            'arrive_hour' => $request->arrive_hour,
+            'leave_hour' => $request->leave_hour
+        ]);
+
+        return response()->json(["success" => true, 'estate' => $estate]);
+    }
+
+
+    public function delete(string $id): ?JsonResponse
+    {
+        $estate = Estate::findOrFail($id);
+
+        $estate->delete();
+
+        return response()->json(["success" => true]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,22 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\EstatesController;
 use Illuminate\Support\Facades\Route;
+
+/*
+API requests:
+| '/' => 'index'
+| '/real_estates' => 'All real_estates'
+| '/real_estates/{id}' => 'real_estate with id'
+| '/real_estates/create' => 'Create real estate'
+| '/real_estates/edit/{id}' => 'Update real estate with id'
+| '/real_estates/delete/{id}' => 'Delete real estate with id'
+|
+| 'register' => 'Register user'
+| 'login' => 'Login user
+*/
+
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
@@ -13,3 +28,13 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 Route::get('/list-categories', [CategoriesController::class, 'getCategories']);
+
+Route::get('/real_estates', [EstatesController::class, 'list']);
+
+Route::get('/real_estates/{id}', [EstatesController::class, 'show']);
+
+Route::post('/real_estates/create', [EstatesController::class, 'store']);
+
+Route::put('/real_estates/edit/{id}', [EstatesController::class, 'update']);
+
+Route::delete('/real_estates/delete/{id}', [EstatesController::class, 'delete']);
