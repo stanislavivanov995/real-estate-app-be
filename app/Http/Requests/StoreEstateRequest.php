13d5dbf84cdcea38230a496bd107869cbd619f3f
Kosta Kazakov <kostakazakoff@gmail.com>
2023-11-14 15:11:05 +0200

    field 'description' changed to nullable

--- app/Http/Requests/StoreEstateRequest.php
+++ app/Http/Requests/StoreEstateRequest.php
@@ -20,17 +20,14 @@ class StoreEstateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'user_id' => 'required | integer',
-            'name' => 'required | string | min:2 | max:100',
-            'description' => 'string',
-            'price' => 'required | numeric | min:0',
-            'currency' => 'required | string',
-            'latitude' => 'string',
-            'longtitude' => 'string',
-            'category' => 'required | numeric | min:1 | max:5',
-            'rooms' => 'required | numeric | min:1',
-            'arrive_hour' => 'required | string',
-            'leave_hour' => 'required | string',
+            'user_id' => 'required',
+            'name' => 'required',
+            'price' => 'required',
+            'currency' => 'required',
+            'category' => 'required',
+            'rooms' => 'required',
+            'arrive_hour' => 'required',
+            'leave_hour' => 'required',
         ];
     }
 
@@ -53,16 +50,10 @@ class StoreEstateRequest extends FormRequest
         return [
             'user_id.required' => 'User id field is required',
             'name.required' => 'Title field is required',
-            'name.min' => 'Title must contain minimum 2 characters',
-            'name.max' => 'Title must contain maximum 100 characters',
             'price.required' => 'Price field is required',
-            'price.min:0' => 'Price cannot be negative',
             'currency.required' => 'Currency field is required',
             'category.required' => 'Category field is required',
-            'category.min:1' => 'Category id must be between 1 and 5',
-            'category.max:5' => 'Category id must be between 1 and 5',
             'rooms.required' => 'Rooms field is required',
-            'rooms.min:1' => 'Rooms cannot be less than 1',
             'arrive_hour.required' => 'Arrive Hour field is required',
             'leave_hour.required' => 'Leave Hour field is required'
         ];
