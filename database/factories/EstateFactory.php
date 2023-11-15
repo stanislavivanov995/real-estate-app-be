26fb714e4f25beeb2d039bbb64f720ab4f6a42db
Kosta Kazakov <kostakazakoff@gmail.com>
2023-11-15 15:10:03 +0200

    change seeder categories (1, 5)

--- database/factories/EstateFactory.php
+++ database/factories/EstateFactory.php
@@ -17,7 +17,7 @@ class EstateFactory extends Factory
             'currency' => fake()->currencyCode,
             'latitude' => fake()->creditCardNumber,
             'longtitude' => fake()->creditCardNumber,
-            'category_id' => fake()->numberBetween(1, 4),
+            'category_id' => fake()->numberBetween(1, 5),
             'arrive_hour' => fake()->time,
             'leave_hour' => fake()->time
         ];
