91f3e373f460c30b61b58585d73ba9694f143ad0
Kosta Kazakov <kostakazakoff@gmail.com>
2023-11-15 15:44:53 +0200

    accept  AuthController incomming changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,41 +2,6 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
-use APP\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Http\JsonResponse;
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-
-class AuthController extends Controller
-{
-    public function user() {
-        return 'Successfully';
-    }
-
-    public function register(Request $request)
-    {
-        $user = USER::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password) 
-        ]);
-
-        return response()->json($user);
-    }
-
-    public function login(Request $request) {
-        if(!Auth::attempt($request->only('email', 'password'))){
-            return response()->json([
-                'message' => 'Login failed',
-            ], Response::HTTP_UNAUTHORIZED);
-        }
-    }
-}
-=======
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -89,4 +54,3 @@ class AuthController extends Controller
         ])->withCookie($cookie);
     }
 }
->>>>>>> main
