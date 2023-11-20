d4501dac9c2fb13138e8b961f11e6167de256ab8
Kosta Kazakov <kostakazakoff@gmail.com>
2023-11-20 13:02:23 +0200

    Add Category::truncate() to CategorySeeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -10,6 +10,8 @@ class CategorySeeder extends Seeder
 {
     public function run(): void
     {
+        Category::truncate();
+
         Category::create([
             'name' => 'Hotel',
         ]);
