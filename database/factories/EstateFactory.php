4de17cdb0f3bb7fe114d5e1a047f355befd76f9f
Kosta Kazakov <kostakazakoff@gmail.com>
2023-11-21 21:28:40 +0200

    EstateFactory created

---
+++ database/factories/EstateFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class EstateFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'user_id' => random_int(1, 10000),
+            'name' => fake()->company(),
+            'description' => fake()->text,
+            'rooms' => fake()->randomDigitNotNull,
+            'price' => fake()->randomDigitNotNull,
+            'currency' => fake()->randomElement(['BGN', 'USD', 'EUR']),
+            'latitude' => fake()->latitude(),
+            'longitude' => fake()->longitude(),
+            'category_id' => fake()->numberBetween(1, 5),
+            'arrive_hour' => fake()->time,
+            'leave_hour' => fake()->time
+        ];
+    }
+}
