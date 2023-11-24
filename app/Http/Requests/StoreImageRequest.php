4cd1f0938f78aab25b72458579dd004a9df582f7
Kosta Kazakov <kostakazakoff@gmail.com>
2023-11-24 17:19:44 +0200

    Images validation edited

--- app/Http/Requests/StoreImageRequest.php
+++ app/Http/Requests/StoreImageRequest.php
@@ -15,14 +15,15 @@ class StoreImageRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'images.*' => 'required|mimes:png,jpg,jpeg|max:2048',
+            'images' => 'required',
+            'images.*' => 'mimes:png,jpg,jpeg|max:2048',
         ];
     }
 
     public function messages()
     {
         return [
-            'images.*.required' => 'At least one image is required',
+            'images.required' => 'At least one image is required',
             'images.*.mimes' => 'Available file formats are png, jpg, jpeg',
             'images.*.max' => 'Maximum file size is 2MB',
         ];
