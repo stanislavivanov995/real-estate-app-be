a220b190fa0bdaa84341bcc9678c0379379f81d4
Kosta Kazakov <kostakazakoff@gmail.com>
2023-11-25 12:55:16 +0200

    comment removed

--- app/Http/Controllers/EstatesController.php
+++ app/Http/Controllers/EstatesController.php
@@ -126,10 +126,7 @@ class EstatesController extends Controller
         return response()->json(["success" => true]);
     }
 
-
-    /*
-    TODO: Delete controller?
-    */
+    
     public function getEstateReservations(string $id): JsonResponse
     {
         try {
