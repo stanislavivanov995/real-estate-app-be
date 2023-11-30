ffed50cffcbe6a7a0222b2a499cbca2f1a3d0c17
Kosta Kazakov <kostakazakoff@gmail.com>
2023-11-30 13:44:32 +0200

    images validation edited - must fix it

--- app/Http/Requests/StoreImageRequest.php
+++ app/Http/Requests/StoreImageRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 
 /* TODO:  */
@@ -16,17 +18,27 @@ class StoreImageRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'images' => 'required',
+            // 'images' => 'required',
             // 'images.*' => 'mimes:png,jpg,jpeg|max:2048',
         ];
+    }
+    
+    public function failedValidation(Validator $validator)
+    {
+        $result = array('Errors' => [
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]);
+        throw new HttpResponseException(response()->json($result));
     }
 
     public function messages()
     {
         return [
-            'images.required' => 'At least one image is required',
+            // 'images.required' => 'At least one image is required',
             // 'images.*.mimes' => 'Available file formats are png, jpg, jpeg',
-            'images.*.max' => 'Maximum file size is 2MB',
+            // 'images.*.max' => 'Maximum file size is 2MB',
         ];
     }
 }
