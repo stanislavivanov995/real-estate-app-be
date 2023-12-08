e2597106feec9e94ce93fbf2d8fa5a8890f76fa7
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-12-08 15:54:11 +0200

    Created migration for user info

---
+++ database/migrations/2023_12_08_130745_create_user_infos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_infos', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->nullable();
+            $table->string('avatar')->nullable();
+            $table->longText('bio')->nullable();
+            $table->string('phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_infos');
+    }
+};
